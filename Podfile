a3e69f4397fd14c89e319d9197bc067bc44c4443
Cameron Bothner <cameronbothner@gmail.com>
2016-08-02 16:00:09 -0400

    Implements titles and descriptions for settings groups.

--- Podfile
+++ Podfile
@@ -1,27 +1,20 @@
 # Uncomment this line to define a global platform for your project
 # platform :ios, '9.0'
 
+def shared_pods
+  pod 'SwiftyJSON', '~> 2.3.2'
+end
+
 target 'player' do
-  # Comment this line if you're not using Swift and don't want to use dynamic frameworks
+
   use_frameworks!
 
-  # Pods for player
-  pod 'SwiftyJSON', '~> 2.3.2'
+  shared_pods
   pod 'pop'
-
-  target 'playerTests' do
-    inherit! :search_paths
-    # Pods for testing
-  end
-
-  target 'playerUITests' do
-    inherit! :search_paths
-    # Pods for testing
-  end
 
 end
 
 target 'todaySchedule' do
   use_frameworks!
-  pod 'SwiftyJSON', '~> 2.3.2'
+  shared_pods
 end
--- player/SettingsTableViewController.swift
+++ player/SettingsTableViewController.swift
@@ -10,38 +10,50 @@ import UIKit
 import MessageUI
 
 class SettingsTableViewController: UITableViewController, MFMessageComposeViewControllerDelegate {
-
-  private let settings: [String: [[Setting]]] = [
+  
+  private let settings: [String: [SettingsGroup]] = [
     "Settings": [
-      [
+      SettingsGroup(settings: [
         Setting(message: "Stream Quality", accessoryType: .DisclosureIndicator),
         Setting(message: "“Save Song” Service", accessoryType: .DisclosureIndicator)
-      ],[
+        ]),
+      SettingsGroup(settings: [
         Setting(key: "call", message: "Call the studio"),
         Setting(key: "text", message: "Send an iMessage to the DJ"),
-        Setting(key: "donate", message: "Give WCBN a buck or two"),
-      ],[
-        Setting(key: "about", message: "About WCBN"),
+        Setting(key: "Give to WCBN", message: "Give WCBN a buck or two"),
+        ]),
+      SettingsGroup(settings: [
+        Setting(message: "About WCBN"),
         Setting(key: "review", message: "Write a review of our app")
-      ]
+        ])
     ],
-    "Stream Quality" : [
-      [
+    "Stream Quality": [
+      SettingsGroup(instructions: "Choose a stream quality.", settings: [
         Setting(key: WCBNStream.URL.high, message: WCBNStream.nameFromURL[WCBNStream.URL.high]!),
         Setting(key: WCBNStream.URL.medium, message: WCBNStream.nameFromURL[WCBNStream.URL.medium]!),
         Setting(key: WCBNStream.URL.low, message: WCBNStream.nameFromURL[WCBNStream.URL.low]!),
-      ]
+        ])
     ],
-    "“Save Song” Service" : [
+    "“Save Song” Service": [SettingsGroup(instructions: "Choose which service you’d like to use to save any new favorites you hear on WCBN. You can choose to purchase songs from the iTunes music library, or add them to a Spotify playlist. You can only save songs that are in the library of your chosen service.", settings:
       SongSearchServiceChoice.allChoices.map { choice in
         return Setting(message: choice.rawValue)
-      }
-    ]
+      })
+    ],
+    "Give to WCBN": [
+      SettingsGroup(instructions: "Thank you for considering supporting WCBN. Gifts listed above are one-time “In-App Purchases” which support the station.", settings: [
+        Setting(key: "1USD", message: "This song is pretty good.", description: "$0.99"),
+        Setting(key: "2USD", message: "Wow, this DJ has such personality!", description: "$1.99"),
+        Setting(key: "5USD", message: "Can I implant a receiver in my brain yet?", description: "$4.99"),
+        ]),
+      SettingsGroup(instructions: "You may give any amount, and claim the donation as tax-exempt, by donating through the University of Michigan’s giving portal.", settings: [
+        Setting(key: "UM OUD", message: "Make a tax-exempt gift of any amount"),
+        ])
+    ],
+    "About WCBN": [
+      SettingsGroup(title: "Freeing your mind since 1972", instructions: "Earliest history of WCBN.\n"),
+      SettingsGroup(title: "Personal Radio", instructions: "WCBN DJs always make their own choices.\n"),
+    ],
   ]
-
-
-
-
 
   @IBOutlet weak var navTitle: UINavigationItem!
 
@@ -51,11 +63,23 @@ class SettingsTableViewController: UITableViewController, MFMessageComposeViewCo
     static let CellReuseIdentifier = "Setting"
   }
 
+  private struct SettingsGroup {
+    let title: String?
+    let instructions: String?
+    let settings: [Setting]
+    init(title: String? = nil, instructions: String? = nil, settings: [Setting] = []) {
+      self.title = title
+      self.instructions = instructions
+      self.settings = settings
+    }
+  }
+
   private struct Setting {
     let delegate = UIApplication.sharedApplication().delegate as! AppDelegate
     var key: String
     var message: String
     var realAccessoryType: UITableViewCellAccessoryType
+    var description: String?
     var accessoryType: UITableViewCellAccessoryType {
       get {
         if (realAccessoryType == .None) && (
@@ -72,22 +96,15 @@ class SettingsTableViewController: UITableViewController, MFMessageComposeViewCo
       }
     }
 
-    init(key: String, message: String, accessoryType: UITableViewCellAccessoryType) {
+    init(key: String, message: String, accessoryType: UITableViewCellAccessoryType = .None, description: String? = nil) {
       self.key = key
       self.message = message
       self.realAccessoryType = accessoryType
+      self.description = description
     }
 
-    init(key: String, message: String) {
-      self.init(key: key, message: message, accessoryType: .None)
-    }
-
-    init(message: String, accessoryType: UITableViewCellAccessoryType) {
+    init(message: String, accessoryType: UITableViewCellAccessoryType = .None) {
       self.init(key: message, message: message, accessoryType: accessoryType)
-    }
-
-    init(message: String) {
-      self.init(key: message, message: message, accessoryType: .None)
     }
   }
 
@@ -103,6 +120,9 @@ class SettingsTableViewController: UITableViewController, MFMessageComposeViewCo
       NSForegroundColorAttributeName: UIColor.blackColor()
     ]
     navTitle.title = settingsGroup
+
+    self.tableView.sectionFooterHeight = UITableViewAutomaticDimension;
+    self.tableView.estimatedSectionFooterHeight = 100;
   }
 
   override func viewWillAppear(animated: Bool) {
@@ -115,25 +135,28 @@ class SettingsTableViewController: UITableViewController, MFMessageComposeViewCo
   }
 
   override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    return settings[settingsGroup]![section].count
+    return settings[settingsGroup]![section].settings.count
   }
 
   override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
     let cell = tableView.dequeueReusableCellWithIdentifier(Storyboard.CellReuseIdentifier, forIndexPath: indexPath)
 
-    let setting = settings[settingsGroup]![indexPath.section][indexPath.row]
+    let setting = settings[settingsGroup]![indexPath.section].settings[indexPath.row]
 
     // Configure the cell...
     cell.textLabel?.text = setting.message
 
     let detail: String
-    switch setting.key {
-    case "Stream Quality":
-      detail = WCBNStream.nameFromURL[delegate.streamURL]!
-    case "“Save Song” Service":
-      detail = delegate.songSearchService.name
-    default:
-      detail = ""
+    if let description = setting.description { detail = description }
+    else {
+      switch setting.key {
+      case "Stream Quality":
+        detail = WCBNStream.nameFromURL[delegate.streamURL]!
+      case "“Save Song” Service":
+        detail = delegate.songSearchService.name
+      default:
+        detail = ""
+      }
     }
     cell.detailTextLabel?.text = detail
 
@@ -142,8 +165,39 @@ class SettingsTableViewController: UITableViewController, MFMessageComposeViewCo
     return cell
   }
 
+  override func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+    return settings[settingsGroup]![section].title
+  }
+
+  override func tableView(tableView: UITableView, viewForFooterInSection section: Int) -> UIView? {
+    let label = labelForFooter(section)
+    let v = UIView()
+    v.preservesSuperviewLayoutMargins = true
+    v.addSubview(label)
+
+    label.topAnchor.constraintEqualToAnchor(v.topAnchor, constant: 8).active = true
+    label.bottomAnchor.constraintEqualToAnchor(v.bottomAnchor, constant: 4).active = true
+
+    let guide = v.readableContentGuide
+    label.translatesAutoresizingMaskIntoConstraints = false
+    guide.leftAnchor.constraintEqualToAnchor(label.leftAnchor).active = true
+    guide.rightAnchor.constraintEqualToAnchor(label.rightAnchor).active = true
+
+    return v
+  }
+
+  func labelForFooter(section: Int) -> UILabel {
+    let label = UILabel()
+    label.text = settings[settingsGroup]![section].instructions
+    label.font = UIFont(name: "Lato-Regular", size: 15)
+    label.numberOfLines = 0
+    label.preservesSuperviewLayoutMargins = true
+    label.sizeToFit()
+    return label
+  }
+
   override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
-    let action = settings[settingsGroup]![indexPath.section][indexPath.row]
+    let action = settings[settingsGroup]![indexPath.section].settings[indexPath.row]
 
     switch action.key {
 
