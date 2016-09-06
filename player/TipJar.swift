29e0d1e796db52d5d3ed57adcaee63746e12c34d
Cameron Bothner <cameronbothner@gmail.com>
2016-09-06 14:02:04 -0400

    Fixes the four finger swipe bug and refactors IAP—maybe they'll pass review :D

--- player/TipJar.swift
+++ player/TipJar.swift
@@ -8,40 +8,110 @@
 
 import Foundation
 import StoreKit
+import SwiftyJSON
 
-class TipJarTableViewController: UITableViewController, SKProductsRequestDelegate {
-  var products: [SKProduct] = []
+class TipJar: NSObject, SKProductsRequestDelegate, SKPaymentTransactionObserver {
+
+  static var sharedInstance = TipJar()
+
+  var uid: String {
+    get {
+      let defaults = NSUserDefaults.standardUserDefaults()
+      if let savedUID = defaults.stringForKey("UID") {
+        return savedUID
+      }
+      let newUID = UIDevice.currentDevice().identifierForVendor!.UUIDString
+      defaults.setObject(newUID, forKey: "UID")
+      return newUID
+    }
+  }
 
   override init() {
     super.init()
-    var request = SKProductsRequest(productIdentifiers: tipNames)
+
+    let request = SKProductsRequest(productIdentifiers: TipJar.tipNames)
     request.delegate = self
     request.start()
+
+    SKPaymentQueue.defaultQueue().addTransactionObserver(self)
   }
 
+  // MARK: - SKProductRequestDelegate
+
+  var products: [SKProduct] = []
+
   static var tipNames = Set([
-    "org.wcbn.smallTip",
-    "org.wcbn.mediumTip",
-    "org.wcbn.largeTip"
+    "org.wcbn.tip1",
+    "org.wcbn.tip2",
+    "org.wcbn.tip5"
   ])
 
-  var tipSettings: [SettingsTableViewController.Setting] {
-    get {
-      return products.map { product in
-        let numberFormatter = NSNumberFormatter()
-        numberFormatter.formatterBehavior = .Behavior10_4
-        numberFormatter.numberStyle = .CurrencyStyle
-        numberFormatter.locale = product.priceLocale
+  @objc func productsRequest(request: SKProductsRequest, didReceiveResponse response: SKProductsResponse) {
+    products = response.products.sort { a, b in a.price.compare(b.price) == .OrderedAscending }
+  }
 
-        return SettingsTableViewController.Setting(
-          key: product.productIdentifier,
-          message: product.localizedTitle,
-          description: numberFormatter.stringFromNumber(product.price))
+  // MARK: - SKProductsRequestDelegate
+
+  @objc func paymentQueue(queue: SKPaymentQueue, updatedTransactions transactions: [SKPaymentTransaction]) {
+    for transaction in transactions {
+      switch transaction.transactionState {
+      case .Purchased:
+        SKPaymentQueue.defaultQueue().finishTransaction(transaction)
+        askForName(transaction)
+      case .Failed:
+        SKPaymentQueue.defaultQueue().finishTransaction(transaction)
+      default:
+        break
       }
     }
   }
 
-  @objc func productsRequest(request: SKProductsRequest, didReceiveResponse response: SKProductsResponse) {
-    products = response.products
+  func askForName(transaction: SKPaymentTransaction) {
+    let alert = UIAlertController(title: "Thank you!", message: "We’re so pleased you like what we’re doing. If you would like to let the DJ know who tipped, please give your name below.", preferredStyle: .Alert)
+
+    let anonymize = UIAlertAction(title: "Keep it anonymous", style: .Cancel) { _ in
+      self.recordDonation(transaction)
+    }
+    alert.addAction(anonymize)
+
+    let shoutout = UIAlertAction(title: "Tell the DJ", style: .Default) { _ in
+      let nameField = alert.textFields![0]
+      let messageField = alert.textFields![1]
+      self.recordDonation(transaction, name: nameField.text, message: messageField.text)
+    }
+    shoutout.enabled = false
+    alert.addAction(shoutout)
+
+    alert.addTextFieldWithConfigurationHandler { textField in
+      textField.autocapitalizationType = .Words
+      textField.autocorrectionType = .Yes
+      textField.placeholder = "Name"
+      NSNotificationCenter.defaultCenter().addObserverForName(UITextFieldTextDidChangeNotification, object: textField, queue: NSOperationQueue.mainQueue()) { (notification) in
+        shoutout.enabled = textField.text != ""
+      }
+    }
+    alert.addTextFieldWithConfigurationHandler { textField in
+      textField.autocapitalizationType = .Sentences
+      textField.autocorrectionType = .Yes
+      textField.placeholder = "Message"
+    }
+
+    let delegate = UIApplication.sharedApplication().delegate as? AppDelegate
+    let rVC = delegate?.window?.rootViewController
+    rVC?.presentViewController(alert, animated: true, completion: nil)
   }
+
+  func recordDonation(transaction: SKPaymentTransaction, name: String? = nil, message: String? = nil) {
+    let receiptURL = NSBundle.mainBundle().appStoreReceiptURL!
+    guard let receipt = NSData(contentsOfURL: receiptURL) else { return }
+    let body: JSON = ["tip": [
+                        "receipt_data": receipt.base64EncodedStringWithOptions(.Encoding64CharacterLineLength),
+                        "product_id": transaction.payment.productIdentifier,
+                        "uid": uid,
+                        "name": name ?? "",
+                        "message": message ?? ""]]
+    let hdr = ["Content-Type": "application/json"]
+    hit(NSURL(string: "https://app.wcbn.org/tips")!, containingBody: body, withHeaders: hdr, using: "POST") { _ in }
+  }
+
 }
--- player/TipJarTableViewController.swift
+++ player/TipJarTableViewController.swift
@@ -10,28 +10,12 @@ import UIKit
 import StoreKit
 import SwiftyJSON
 
-class TipJarTableViewController: UITableViewController, SKProductsRequestDelegate, SKPaymentTransactionObserver {
+class TipJarTableViewController: UITableViewController {
 
-  var uid: String {
-    get {
-      let defaults = NSUserDefaults.standardUserDefaults()
-      if let savedUID = defaults.stringForKey("UID") {
-        return savedUID
-      }
-      let newUID = UIDevice.currentDevice().identifierForVendor!.UUIDString
-      defaults.setObject(newUID, forKey: "UID")
-      return newUID
-    }
-  }
+  let products = TipJar.sharedInstance.products
 
   override func viewDidLoad() {
     super.viewDidLoad()
-
-    let request = SKProductsRequest(productIdentifiers: TipJarTableViewController.tipNames)
-    request.delegate = self
-    request.start()
-
-    SKPaymentQueue.defaultQueue().addTransactionObserver(self)
 
     tableView.sectionFooterHeight = UITableViewAutomaticDimension;
     tableView.estimatedSectionFooterHeight = 1000;
@@ -40,21 +24,6 @@ class TipJarTableViewController: UITableViewController, SKProductsRequestDelegat
   override func didReceiveMemoryWarning() {
     super.didReceiveMemoryWarning()
     // Dispose of any resources that can be recreated.
-  }
-
-  // MARK: - SKProductRequestDelegate
-
-  var products: [SKProduct] = []
-
-  static var tipNames = Set([
-    "org.wcbn.tip1",
-    "org.wcbn.tip2",
-    "org.wcbn.tip5"
-  ])
-
-  @objc func productsRequest(request: SKProductsRequest, didReceiveResponse response: SKProductsResponse) {
-    products = response.products.sort { a, b in a.price.compare(b.price) == .OrderedAscending }
-    tableView.reloadData()
   }
 
   // MARK: - Table view data source
@@ -145,72 +114,11 @@ class TipJarTableViewController: UITableViewController, SKProductsRequestDelegat
     if indexPath.section == 1 {
       let giftURL = NSURL(string: "https://leadersandbest.umich.edu/find/#!/give/basket/fund/361991")
       UIApplication.sharedApplication().openURL(giftURL!)
-      deselectSelectedRow()
     } else if products != [] {
       let payment = SKPayment(product: products[indexPath.row])
       SKPaymentQueue.defaultQueue().addPayment(payment)
     }
-  }
-
-  func paymentQueue(queue: SKPaymentQueue, updatedTransactions transactions: [SKPaymentTransaction]) {
-    for transaction in transactions {
-      switch transaction.transactionState {
-      case .Purchased:
-        SKPaymentQueue.defaultQueue().finishTransaction(transaction)
-        askForName(transaction)
-      case .Failed:
-        SKPaymentQueue.defaultQueue().finishTransaction(transaction)
-        deselectSelectedRow()
-      default:
-        break
-      }
-    }
-  }
-
-  func askForName(transaction: SKPaymentTransaction) {
-    let alert = UIAlertController(title: "Thank you!", message: "We’re so pleased you like what we’re doing. If you would like to let the DJ know who tipped, please give your name below.", preferredStyle: .Alert)
-
-    let anonymize = UIAlertAction(title: "Keep it anonymous", style: .Cancel) { _ in
-      self.recordDonation(transaction)
-    }
-    alert.addAction(anonymize)
-
-    let shoutout = UIAlertAction(title: "Tell the DJ", style: .Default) { _ in
-      let nameField = alert.textFields![0]
-      let messageField = alert.textFields![1]
-      self.recordDonation(transaction, name: nameField.text, message: messageField.text)
-    }
-    shoutout.enabled = false
-    alert.addAction(shoutout)
-
-    alert.addTextFieldWithConfigurationHandler { textField in
-      textField.autocapitalizationType = .Words
-      textField.autocorrectionType = .Yes
-      textField.placeholder = "Name"
-      NSNotificationCenter.defaultCenter().addObserverForName(UITextFieldTextDidChangeNotification, object: textField, queue: NSOperationQueue.mainQueue()) { (notification) in
-        shoutout.enabled = textField.text != ""
-      }
-    }
-    alert.addTextFieldWithConfigurationHandler { textField in
-      textField.autocapitalizationType = .Sentences
-      textField.autocorrectionType = .Yes
-      textField.placeholder = "Message"
-    }
-
-    presentViewController(alert, animated: true, completion: deselectSelectedRow)
-  }
-
-  func recordDonation(transaction: SKPaymentTransaction, name: String? = nil, message: String? = nil) {
-    let receiptURL = NSBundle.mainBundle().appStoreReceiptURL!
-    guard let receipt = NSData(contentsOfURL: receiptURL) else { return }
-    let body: JSON = ["tip": [
-                        "receipt_data": receipt.base64EncodedStringWithOptions(.Encoding64CharacterLineLength),
-                        "product_id": transaction.payment.productIdentifier,
-                        "uid": uid,
-                        "name": name ?? "",
-                        "message": message ?? ""]]
-    let hdr = ["Content-Type": "application/json"]
-    hit(NSURL(string: "https://app.wcbn.org/tips")!, containingBody: body, withHeaders: hdr, using: "POST") { _ in }
+    deselectSelectedRow()
   }
 
   func deselectSelectedRow() {
